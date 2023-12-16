cc26b1900de2d4c952538607951a9f0d254e63d3
tkl0000 <thomasli2025@gmail.com>
2023-12-16 15:40:43 -0500

    preload profile

--- frontend/src/app/profile/[slug]/page.tsx
+++ frontend/src/app/profile/[slug]/page.tsx
@@ -8,10 +8,27 @@ import * as React from "react";
 import { useRouter } from 'next/router';
 import { usePathname } from 'next/navigation';
 
+const meetupSample =   {
+  id: 1,
+  title: 'Boost your conversion rate',
+  description:
+    'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+  date: 'Mar 16, 2020',
+  datetime: '2020-03-16',
+  imageUrl: "https://s3-media0.fl.yelpcdn.com/bphoto/55ytTjhnJ8ka0O4ccaV6aA/o.jpg",
+  author: {
+    id: 1,
+    name: 'Michael Foster',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+}
+
 export default function Post() {
-  const [firstName, setFirstName] = useState<string>("First");
-  const [lastName, setLastName] = useState<string>("Last");
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
   const [userBio, setBio] = useState<string>("...");
+  const [userImage, setImage] = useState<string>("...")
   const badSlug = usePathname();
   const slug = badSlug.split("/")[badSlug.split("/").length - 1]
   console.log(slug)
@@ -20,7 +37,9 @@ export default function Post() {
     fetch("/api/users/" + slug)
       .then(response => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          setFirstName(meetupSample.author.name)
+          setBio(meetupSample.description)
+          setImage(meetupSample.author.imageUrl)
         }
         return response.json();
       })
@@ -61,8 +80,7 @@ export default function Post() {
               <div className='col-span-2 col-start-4'>
                 <label
                   htmlFor='title'
-                  className='block text-sm font-medium leading-6 text-gray-900'
-                >
+                  className='block text-sm font-medium leading-6 text-gray-900'>
                   <h1>
                     {firstName} {lastName}
                   </h1>
@@ -71,10 +89,8 @@ export default function Post() {
 
               <div className='col-span-1'>
                 <div className='m-2 flex items-center gap-x-3 float-right'>
-                  <PhotoIcon
-                    className='h-12 w-12 text-gray-300'
-                    aria-hidden='true'
-                  />
+                  <img className='h-12 w-12 rounded-full' src={userImage}>
+                  </img>
                 </div>
               </div>
 
