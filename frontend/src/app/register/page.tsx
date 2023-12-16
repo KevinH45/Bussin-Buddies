027f01b8b6d968fb2cd8a172895f2d0e0f238486
Vlad Bondar <vlad@vladb.xyz>
2023-12-16 11:18:39 -0500

    register

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -3,11 +3,11 @@
 import Head from 'next/head';
 import * as React from 'react';
 
-export default function Login() {
+export default function Register() {
     return (
         <main className="h-full">
             <Head>
-                <title>Login</title>
+                <title>Register</title>
             </Head>
             <div className="flex min-h-full flex-1 items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
                 <div className="w-full max-w-sm space-y-10">
@@ -18,12 +18,43 @@ export default function Login() {
                             alt="Your Company"
                         />
                         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                            Sign in to your account
+                            Register
                         </h2>
                     </div>
                     <form className="space-y-6" action="#" method="POST">
                         <div className="relative -space-y-px rounded-md shadow-sm">
-                            <div className="pointer-events-none absolute inset-0 z-10 rounded-md ring-1 ring-inset ring-gray-300" />
+                            <div
+                                className="pointer-events-none absolute inset-0 z-10 rounded-md ring-1 ring-inset ring-gray-300"/>
+                            <div className="flex flex-row">
+                                <div>
+                                    <label htmlFor="first-name" className="sr-only">
+                                        First Name
+                                    </label>
+                                    <input
+                                        id="first-name"
+                                        name="firstname"
+                                        type="text"
+                                        autoComplete="none"
+                                        required
+                                        className="relative block rounded-t-md w-full border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        placeholder="First Name"
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="last-name" className="sr-only">
+                                        Last Name
+                                    </label>
+                                    <input
+                                        id="last-name"
+                                        name="lastname"
+                                        type="text"
+                                        autoComplete="none"
+                                        required
+                                        className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        placeholder="Last Name"
+                                    />
+                                </div>
+                            </div>
                             <div>
                                 <label htmlFor="email-address" className="sr-only">
                                     Email address
@@ -34,8 +65,22 @@ export default function Login() {
                                     type="email"
                                     autoComplete="email"
                                     required
-                                    className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="relative block w-full border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     placeholder="Email address"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="email-address" className="sr-only">
+                                    Username
+                                </label>
+                                <input
+                                    id="username"
+                                    name="username"
+                                    type="text"
+                                    autoComplete="username"
+                                    required
+                                    className="relative block w-full border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    placeholder="Username"
                                 />
                             </div>
                             <div>
@@ -85,9 +130,9 @@ export default function Login() {
                     </form>
 
                     <p className="text-center text-sm leading-6 text-gray-500">
-                        Not a member?{' '}
+                        Already a Member?{' '}
                         <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                            Register CHANGEME NEXT LINKN
+                            Login CHANGEMENEXTLINK
                         </a>
                     </p>
                 </div>
