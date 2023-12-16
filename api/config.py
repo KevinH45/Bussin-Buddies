0135460fef6fdce72b94e571258cfd2fc61832bb
KevinH45 <timmyhwang127@gmail.com>
2023-12-16 16:49:43 -0500

    fsadijjf

--- api/config.py
+++ api/config.py
@@ -1,7 +1,8 @@
+from datetime import timedelta
 class Config:
 
     DEBUG = True
     JWT_SECRET_KEY = "super-secret"
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
-
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
