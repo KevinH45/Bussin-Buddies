c35f1d0bf8783a08796a9f06fcee988901c66466
KevinH45 <timmyhwang127@gmail.com>
2023-12-16 12:28:34 -0500

    added app.py

---
+++ api/app.py
@@ -0,0 +1,52 @@
+from flask_cors import CORS
+from flask import Flask
+from flask_restful import Api
+from extensions import jwt
+import firebase_admin
+from config import Config
+from resources.auth import *
+from resources.listings import *
+from resources.user import *
+
+def register_extensions(app):
+    CORS(app)
+    jwt.init_app(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blacklist(header, token):
+        jti = token['jti']
+        return jti in jwt_blacklist
+
+
+def create_app():
+    app = Flask("lasdfa")
+    app.config.from_object(Config)
+
+    register_extensions(app)
+    register_resources(app)
+
+    return app
+
+
+def register_resources(app):
+    api = Api(app)
+
+    # Auth
+    api.add_resource(LoginResource, "/api/users/login")
+    api.add_resource(LogoutResource, "/api/users/logout")
+    api.add_resource(RefreshResource, "/api/users/refresh")
+
+    # User Resources
+    api.add_resource(UserViewResource, "/api/users/<id>")
+    api.add_resource(UserListingsResource, "/api/users/<id>/listings")
+    api.add_resource(UserCreationResource, "/api/users")
+
+    # Listing Resources
+    api.add_resource(ListingSingleResource, "/api/listings/<id>")
+    api.add_resource(ListingMultipleResource, "/api/listings")
+
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
